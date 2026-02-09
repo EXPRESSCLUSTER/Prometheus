801f0ead0cf5bf46cd1da3788b46a482ec112c4d
fukunagt <nec09024785886@docomo.ne.jp>
2026-02-09 12:23:03 +0000

    Remove source files.

--- clpexporter/getelaps.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# set monitor name
-mon="mdw1"
-
-tail -n 1 /opt/nec/clusterpro/perf/cluster/monitor/$mon.cur |awk -F',' '{print $8}'|tr -d '"'
-
--- clpexporter/go.mod
+++
@@ -1,7 +0,0 @@
-module clpexporter
-
-go 1.15
-
-require (
-	github.com/prometheus/client_golang v1.11.1
-)
